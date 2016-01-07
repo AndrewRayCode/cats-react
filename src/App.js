a4006d80b04b3f64fa11b67e62bdafeb9964dba9
Andrew Ray <delvarworld@gmail.com>
2016-01-07 14:57:41 -0800

    Adding another cat

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ const springFrictionFactor = 0.91;
 
 // Potential cats
 const cats = [() => ({
+    height: 375,
+    width: 500,
+    name: 'Captain Fluff\'n\'Stuff',
+    source: 'http://lovemeow.com/2009/12/cute-cats-and-kittens-tongue-sticking-out-pictures/',
     background: {
         img: require( '../kitty-cat-1.jpg' )
     },
@@ -21,7 +25,7 @@ const cats = [() => ({
         top: 195,
         left: 282,
         relativeMovementBounds: {
-            left: 2,
+            left: 5,
             top: 5,
             right: 2,
             bottom: 25
@@ -50,7 +54,60 @@ const cats = [() => ({
         background: '#111521',
         borderColor: '#637790'
     }
+} ), () => ({
+    height: 495,
+    width: 660,
+    name: 'Dorf',
+    source: 'http://www.cutestpaw.com/images/derpy-cat-2/',
+    background: {
+        img: require( '../kitty-cat-2.jpg' )
+    },
+    tongue: {
+        img: require( '../kitty-cat-2-tonue.png' ),
+        top: 265,
+        left: 267,
+        relativeMovementBounds: {
+            left: 2,
+            top: 10,
+            right: 2,
+            bottom: 65
+        }
+    },
+    snoot: {
+        img: require('../kitty-cat-2-snoot.png'),
+        top: 225,
+        left: 114
+    },
+    eye1: {
+        top: 220,
+        left: 197,
+        height: 31,
+        width: 20,
+        travelDistance: 25,
+        background: '#0b1413',
+        borderColor: '#141f19'
+    },
+    eye2: {
+        top: 229,
+        left: 397,
+        height: 31,
+        width: 20,
+        travelDistance: 25,
+        background: '#0b1413',
+        borderColor: '#141f19'
+    }
 } ) ];
+
+const staticCats = cats.map( ( cat ) => cat() );
+
+function boundPositionTo( position, originalPosition, bounds ) {
+
+    return {
+        top: Math.min( Math.max( originalPosition.top - bounds.top, position.top ), originalPosition.top + bounds.bottom ),
+        left: Math.min( Math.max( originalPosition.left - bounds.left, position.left ), originalPosition.left + bounds.right )
+    };
+
+}
 
 function vectorLength2d( vector ) {
 
@@ -86,27 +143,38 @@ function toSpring( x1, y1 ) {
 
 }
 
+function getStateFromIndex( catIndex ) {
+
+    const myCat = cats[ catIndex ]();
+
+    return {
+        isPaused: true,
+        catIndex,
+        cat: myCat,
+        eye1Spring: toSpring( myCat.eye1.left, myCat.eye1.top ),
+        eye2Spring: toSpring( myCat.eye2.left, myCat.eye2.top ),
+        tongueSpring: toSpring( myCat.tongue.left, myCat.tongue.top )
+    };
+
+}
+
 export default class App extends Component {
 
     constructor( props ) {
 
         super( props );
 
-        const myCat = cats[ 0 ]();
+        const state = getStateFromIndex( 1 );
+        state.snootsBooped = parseFloat( localStorage.getItem('boopsSnooted') ) || 0;
 
-        this.state = {
-            isPaused: true,
-            snootsBooped: parseFloat( localStorage.getItem('boopsSnooted') ) || 0,
-            cat: myCat,
-            eye1Spring: toSpring( myCat.eye1.left, myCat.eye1.top ),
-            eye2Spring: toSpring( myCat.eye2.left, myCat.eye2.top ),
-            tongueSpring: toSpring( myCat.tongue.left, myCat.tongue.top )
-        };
+        this.state = state;
 
         this.onAnimate = this.onAnimate.bind( this );
         this.handleDragEnd = this.handleDragEnd.bind( this );
         this.handleDrag = this.handleDrag.bind( this );
         this.handleDragStart = this.handleDragStart.bind( this );
+        this.showBitcoinAddress = this.showBitcoinAddress.bind( this );
+        this.selectCat = this.selectCat.bind( this );
 
     }
 
@@ -119,6 +187,20 @@ export default class App extends Component {
     componentWillUnmount() {
 
         window.cancelAnimationFrame( this.animationId );
+
+    }
+
+    showBitcoinAddress() {
+
+        this.setState({ showBitcoinAddress: !this.state.showBitcoinAddress });
+
+    }
+
+    selectCat( catIndex ) {
+
+        return ( event ) => {
+            this.setState( getStateFromIndex( catIndex ) );
+        };
 
     }
 
@@ -139,8 +221,12 @@ export default class App extends Component {
 
         const { eye1Spring, eye2Spring, tongueSpring, cat } = this.state;
 
+        const boundPosition = boundPositionTo(
+            ui.position, cat.tongue, cat.tongue.relativeMovementBounds
+        );
+
         // How far has this tongue traveled, in + or - %, relative to origin?
-        const tongueYTravelPercent = ( ui.position.top - cat.tongue.top ) / (
+        const tongueYTravelPercent = ( boundPosition.top - cat.tongue.top ) / (
             cat.tongue.relativeMovementBounds.top +
             cat.tongue.relativeMovementBounds.bottom
         );
@@ -153,8 +239,8 @@ export default class App extends Component {
             tongueYTravelPercent * cat.eye2.travelDistance
         );
 
-        tongueSpring.position.x = ui.position.left;
-        tongueSpring.position.y = ui.position.top;
+        tongueSpring.position.x = boundPosition.left;
+        tongueSpring.position.y = boundPosition.top;
 
         this.setState({ cat });
 
@@ -209,14 +295,10 @@ export default class App extends Component {
             totalDistance += distanceFromRest;
 
             // F = - kx - bv
-            // F = -k(|x|-d)(x/|x|) - bv
             const force = {
-                x: -stiffness * distanceFromRest * ( normalVector.x ) - damperFactor * spring.velocity.x,
-                y: -stiffness * distanceFromRest * ( normalVector.y ) - damperFactor * spring.velocity.y
+                x: -stiffness * distanceFromRest * normalVector.x - damperFactor * spring.velocity.x,
+                y: -stiffness * distanceFromRest * normalVector.y - damperFactor * spring.velocity.y
             };
-
-            // block.v += a * frameRate;
-            // block.x += block.v * frameRate;
 
             spring.acceleration.x = timeScale * ( force.x / nodeMass );
             spring.acceleration.y = timeScale * ( force.y / nodeMass );
@@ -233,9 +315,6 @@ export default class App extends Component {
             spring.velocity.x = Math.max( Math.min( spring.velocity.x, velocityMax ), -velocityMax ) * springFrictionFactor;
             spring.velocity.y = Math.max( Math.min( spring.velocity.y, velocityMax ), -velocityMax ) * springFrictionFactor;
 
-            //spring.acceleration.x *= 0.9;
-            //spring.acceleration.y *= 0.9;
-
             spring.position.x += spring.velocity.x;
             spring.position.y += spring.velocity.y;
 
@@ -243,7 +322,7 @@ export default class App extends Component {
 
         });
 
-        const isComplete = totalEnergy < 0.5 && totalDistance < 0.8;
+        const isComplete = totalEnergy < 0.4 && totalDistance < 0.7;
         const state = springs;
 
         if( isComplete ) {
@@ -261,86 +340,123 @@ export default class App extends Component {
 
         const styles = require('./scss/style.scss');
 
-        const { cat, eye1Spring, eye2Spring, tongueSpring } = this.state;
+        const { cat, eye1Spring, eye2Spring, tongueSpring, catIndex } = this.state;
         const { background, tongue, eye1, eye2, snoot } = cat;
 
         return <div className={ styles.appWrap }>
             <div className={ styles.application }>
 
-                <h1 className={ styles.title }>
-                    Booper Snooter
-                </h1>
+                <div className={ styles.header }>
 
-                <div className={ styles.wrap }>
+                    <h1 className={ styles.title }>
+                        Booper Snooter v0.1
+                    </h1>
 
-                    <div className={ styles.container }>
+                    <div className={ styles.snoots }>
+                        Pick a derp:
+                    </div>
+                    <ul className={ styles.catSelector }>
+                        { staticCats.map( ( staticCat, index ) => {
+                            return <li
+                                className={ styles.catSelect + ' ' + ( index === catIndex ? styles.selected : '' ) }
+                                onClick={ this.selectCat( index ) }
+                                key={ index }
+                            >
+                                { staticCat.name }
+                            </li>;
+                        } ) }
+                    </ul>
+                </div>
 
-                        <img
-                            draggable={ false }
-                            className={ styles.image }
-                            src={ background.img }
-                        />
+                <div className={ styles.imageWrap }>
 
-                        <Draggable
-                            ref="draggable"
-                            start={{
-                                x: tongueSpring.position.x,
-                                y: tongueSpring.position.y
+                    <div
+                        className={ styles.container }
+                        style={{
+                            height: `${ cat.height }px`,
+                            width: `${ cat.width }px`,
+                            backgroundImage: `url( ${ background.img } )`
+                        }}
+                    >
+
+                        <div
+                            className={ styles.relative }
+                            style={{
+                                height: `${ cat.height }px`,
+                                width: `${ cat.width }px`
                             }}
-                            bounds={{
-                                left: tongue.left - tongue.relativeMovementBounds.left,
-                                top: tongue.top - tongue.relativeMovementBounds.top,
-                                right: tongue.left + tongue.relativeMovementBounds.right,
-                                bottom: tongue.top + tongue.relativeMovementBounds.bottom
-                            }}
-                            onDrag={ this.handleDrag }
-                            onStop={ this.handleDragEnd }
-                            onStart={ this.handleDragStart }
                         >
+
+                            <Draggable
+                                ref="draggable"
+                                key={ cat.name }
+                                start={{
+                                    x: tongueSpring.position.x,
+                                    y: tongueSpring.position.y
+                                }}
+                                bounds={{
+                                    left: tongue.left - tongue.relativeMovementBounds.left,
+                                    top: tongue.top - tongue.relativeMovementBounds.top,
+                                    right: tongue.left + tongue.relativeMovementBounds.right,
+                                    bottom: tongue.top + tongue.relativeMovementBounds.bottom
+                                }}
+                                onDrag={ this.handleDrag }
+                                onStop={ this.handleDragEnd }
+                                onStart={ this.handleDragStart }
+                            >
+                                <img
+                                    draggable={ false }
+                                    src={ tongue.img }
+                                    className={ styles.noSelect }
+                                    style={{
+                                        position: 'absolute',
+                                        cursor: 'pointer'
+                                    }}
+                                />
+                            </Draggable>
+
                             <img
                                 draggable={ false }
-                                src={ tongue.img }
+                                className={ styles.absolute }
+                                src={ snoot.img }
                                 style={{
-                                    position: 'absolute',
-                                    cursor: 'pointer'
+                                    top: `${ snoot.top }px`,
+                                    left: `${ snoot.left }px`
                                 }}
                             />
-                        </Draggable>
 
-                        <img
-                            draggable={ false }
-                            className={ styles.image }
-                            src={ snoot.img }
-                            style={{
-                                top: `${ snoot.top }px`,
-                                left: `${ snoot.left }px`
-                            }}
-                        />
+                            <div
+                                className={ styles.eye }
+                                style={{
+                                    top: `${ eye1Spring.position.y }px`,
+                                    left: `${ eye1Spring.position.x }px`,
+                                    height: `${ eye1.height }px`,
+                                    width: `${ eye1.width }px`,
+                                    background: `${ eye2.background }`,
+                                    boxShadow: `0 0 2px ${ eye1.borderColor }`
+                                }}
+                            />
 
-                        <div
-                            className={ styles.eye }
-                            style={{
-                                top: `${ eye1Spring.position.y }px`,
-                                left: `${ eye1Spring.position.x }px`,
-                                height: `${ eye1.height }px`,
-                                width: `${ eye1.width }px`,
-                                background: `${ eye2.background }`,
-                                boxShadow: `0 0 2px ${ eye1.borderColor }`
-                            }}
-                        />
+                            <div
+                                className={ styles.eye }
+                                style={{
+                                    top: `${ eye2Spring.position.y }px`,
+                                    left: `${ eye2Spring.position.x }px`,
+                                    height: `${ eye1.height }px`,
+                                    width: `${ eye1.width }px`,
+                                    background: `${ eye2.background }`,
+                                    boxShadow: `0 0 2px ${ eye2.borderColor }`
+                                }}
+                            />
 
-                        <div
-                            className={ styles.eye }
-                            style={{
-                                top: `${ eye2Spring.position.y }px`,
-                                left: `${ eye2Spring.position.x }px`,
-                                height: `${ eye1.height }px`,
-                                width: `${ eye1.width }px`,
-                                background: `${ eye2.background }`,
-                                boxShadow: `0 0 2px ${ eye2.borderColor }`
-                            }}
-                        />
+                        </div>
 
+                    </div>
+
+                    <div className={ styles.imageCredit }>
+                        Image credit <a className={ styles.imageLink } href={ cat.source } rel="nofollow" target="_blank">
+                            { cat.source.substr( 0, 20 ) }&hellip;
+                        </a>
                     </div>
 
                 </div>
@@ -354,7 +470,39 @@ export default class App extends Component {
 
             <div className={ styles.footer }>
                 <div className={ styles.footed }>
-                    By Andrew Ray
+                    <span className={ styles.footSnoot }>by</span>
+                    <a
+                        className={ styles.footLink }
+                        href="http://blog.andrewray.me"
+                        target="_blank"
+                    >
+                        Andrew Ray
+                    </a>
+                    { this.state.showBitcoinAddress ? <pre className={ styles.pre }>
+                        1FsEF4v5vGHiNAgwSBMzBFvbScP96fgohJ
+                    </pre> : <button
+                        onClick={ this.showBitcoinAddress }
+                        className={ styles.donateButton }
+                    >
+                    <img
+                        className={ styles.btcIcon }
+                        src={ require('../coinbase-icon.png') }
+                    /> Donate Bitcoin
+                    </button> }
+                    <a
+                        className={ styles.footLink }
+                        href="https://twitter.com/andrewray"
+                        target="_blank"
+                    >
+                        Twitter
+                    </a>
+                    <a
+                        className={ styles.footLink }
+                        href="https://github.com/DelvarWorld/cats-react"
+                        target="_blank"
+                    >
+                        Github
+                    </a>
                 </div>
             </div>
 
