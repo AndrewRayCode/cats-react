460de64f2b2a20da3e6d2f36487ad236add92547
Andrew Ray <delvarworld@gmail.com>
2016-01-06 20:57:21 -0800

    meow

--- src/App.js
+++ src/App.js
@@ -32,13 +32,13 @@ const cats = [() => ({
     eye1: {
         top: 169,
         left: 267,
-        travelDistance: 7,
+        travelDistance: 9,
         color: '#637790'
     },
     eye2: {
         top: 169,
         left: 327,
-        travelDistance: 7,
+        travelDistance: 9,
         color: '#637790'
     }
 } ) ];
@@ -87,6 +87,7 @@ export default class App extends Component {
         const myCat = cats[ 0 ]();
 
         this.state = {
+            snootsBooped: localStorage.getItem('boopsSnooted') || 0,
             cat: myCat,
             eye1Spring: toSpring( myCat.eye1.left, myCat.eye1.top ),
             eye2Spring: toSpring( myCat.eye2.left, myCat.eye2.top ),
@@ -155,6 +156,10 @@ export default class App extends Component {
 
     handleDragEnd() {
 
+        this.setState({
+            snootsBooped: this.state.snootsBooped + 1
+        });
+ //localStorage.getItem('boopsSnooted') || 0,
         this.isPaused = false;
 
     }
@@ -208,51 +213,6 @@ export default class App extends Component {
             spring.acceleration.x += animationScale * ( force.x / nodeMass );
             spring.acceleration.y += animationScale * ( force.y / nodeMass );
 
-            // force1X = stiffness * ( distance - spring.length ) * ( norm1.x / distance ) - damperFactor * v1.x,
-
-            // our node2 is spring. node1 is origin.
-            // force to apply to node2:
-            // Vector2 F2 = -k * (xAbs - d) * (Vector2.Normalize(node1.p - node2.p) / xAbs) - b * (node2.v - node1.v);
-            
-            // b = coefficient of dampening
-            // d = desired distance of separation
-            
-            //var a = ( F_spring + F_damper ) / block.mass;
-            //block.v += a * frameRate;
-            //block.x += block.v * frameRate;
-
-            //spring.velocity = {
-                //x: spring.velocity.x + Math.max( Math.min(
-                    //force.x / nodeMass, velocityMax
-                //), -velocityMax ) * springFrictionFactor * ( 1 / 60 ),
-                //y: spring.velocity.y + Math.max( Math.min(
-                    //force.y / nodeMass, velocityMax
-                //), -velocityMax ) * springFrictionFactor * ( 1 / 60 )
-            //};
-
-            //totalEnergy += Math.abs( spring.velocity.x ) + Math.abs( spring.velocity.y );
-
-            //spring.position.x += spring.velocity.x * ( 1 / 60 );
-            //spring.position.y += spring.velocity.y * ( 1 / 60 );
-
-
-
-
-            //var F_spring = k * ( (block.x - wall.x) - spring_length );
-            //var F_damper = b * ( block.v - wall.v );
-
-            //var a = ( F_spring + F_damper ) / block.mass;
-            //block.v += a * frameRate;
-            //block.x += block.v * frameRate;
-
-
-            //var F_spring = stiffness * distanceFromRest;
-            //var F_damper = damperFactor * spring.velocity.x;
-
-            //var a = ( F_spring + F_damper ) / nodeMass;
-            //spring.velocity.x += a * ( 1 / 60 );
-            //spring.position.x += spring.velocity.x * ( 1 / 60 );
-
         });
 
         Object.keys( springs ).forEach( ( key ) => {
@@ -275,12 +235,6 @@ export default class App extends Component {
 
         });
 
-        this.xx = this.xx || 1;
-        this.xx++;
-        if( !( this.xx % 19 )  ) {
-            this.setState({ totalEnergy, totalDistance });
-        }
-
         const isComplete = totalEnergy < 0.5 && totalDistance < 0.8;
         const state = { springs };
 
@@ -290,7 +244,6 @@ export default class App extends Component {
 
         this.refs.draggable.state.clientX = springs.tongueSpring.position.x;
         this.refs.draggable.state.clientY = springs.tongueSpring.position.y;
-
 
         this.setState( springs );
 
@@ -303,72 +256,94 @@ export default class App extends Component {
         const { cat, eye1Spring, eye2Spring, tongueSpring } = this.state;
         const { background, tongue, eye1, eye2, snoot } = cat;
 
-        return <div className={ styles.wrap }>
-            { this.state.totalEnergy }...
-            { this.state.totalDistance }
-            <div className={ styles.container }>
+        return <div className={ styles.appWrap }>
+            <div className={ styles.application }>
 
-                <img
-                    draggable={ false }
-                    className={ styles.image }
-                    src={ background.img }
-                />
+                <h1 className={ styles.title }>
+                    Booper Snooter
+                </h1>
 
-                <Draggable
-                    ref="draggable"
-                    start={{
-                        x: tongueSpring.position.x,
-                        y: tongueSpring.position.y
-                    }}
-                    bounds={{
-                        left: tongue.left - tongue.relativeMovementBounds.left,
-                        top: tongue.top - tongue.relativeMovementBounds.top,
-                        right: tongue.left + tongue.relativeMovementBounds.right,
-                        bottom: tongue.top + tongue.relativeMovementBounds.bottom
-                    }}
-                    onDrag={ this.handleDrag }
-                    onStop={ this.handleDragEnd }
-                    onStart={ this.handleDragStart }
-                >
-                    <img
-                        draggable={ false }
-                        src={ tongue.img }
-                        style={{
-                            position: 'absolute',
-                            cursor: 'pointer'
-                        }}
-                    />
-                </Draggable>
+                <div className={ styles.wrap }>
 
-                <img
-                    draggable={ false }
-                    className={ styles.image }
-                    src={ snoot.img }
-                    style={{
-                        top: `${ snoot.top }px`,
-                        left: `${ snoot.left }px`
-                    }}
-                />
+                    <div className={ styles.container }>
 
-                <div
-                    className={ styles.eye }
-                    style={{
-                        top: `${ eye1Spring.position.y }px`,
-                        left: `${ eye1Spring.position.x }px`,
-                        boxShadow: `0 0 2px ${ eye1.color }`
-                    }}
-                />
+                        <img
+                            draggable={ false }
+                            className={ styles.image }
+                            src={ background.img }
+                        />
 
-                <div
-                    className={ styles.eye }
-                    style={{
-                        top: `${ eye2Spring.position.y }px`,
-                        left: `${ eye2Spring.position.x }px`,
-                        boxShadow: `0 0 2px ${ eye2.color }`
-                    }}
-                />
+                        <Draggable
+                            ref="draggable"
+                            start={{
+                                x: tongueSpring.position.x,
+                                y: tongueSpring.position.y
+                            }}
+                            bounds={{
+                                left: tongue.left - tongue.relativeMovementBounds.left,
+                                top: tongue.top - tongue.relativeMovementBounds.top,
+                                right: tongue.left + tongue.relativeMovementBounds.right,
+                                bottom: tongue.top + tongue.relativeMovementBounds.bottom
+                            }}
+                            onDrag={ this.handleDrag }
+                            onStop={ this.handleDragEnd }
+                            onStart={ this.handleDragStart }
+                        >
+                            <img
+                                draggable={ false }
+                                src={ tongue.img }
+                                style={{
+                                    position: 'absolute',
+                                    cursor: 'pointer'
+                                }}
+                            />
+                        </Draggable>
+
+                        <img
+                            draggable={ false }
+                            className={ styles.image }
+                            src={ snoot.img }
+                            style={{
+                                top: `${ snoot.top }px`,
+                                left: `${ snoot.left }px`
+                            }}
+                        />
+
+                        <div
+                            className={ styles.eye }
+                            style={{
+                                top: `${ eye1Spring.position.y }px`,
+                                left: `${ eye1Spring.position.x }px`,
+                                boxShadow: `0 0 2px ${ eye1.color }`
+                            }}
+                        />
+
+                        <div
+                            className={ styles.eye }
+                            style={{
+                                top: `${ eye2Spring.position.y }px`,
+                                left: `${ eye2Spring.position.x }px`,
+                                boxShadow: `0 0 2px ${ eye2.color }`
+                            }}
+                        />
+
+                    </div>
+
+                </div>
+
+                <div className={ styles.snootsBooped }>
+                    <span className={ styles.snoots }>Snoots Booped:</span>
+                    <span className={ styles.booped }> { this.state.snootsBooped }</span>
+                </div>
 
             </div>
+
+            <div className={ styles.footer }>
+                <div className={ styles.footed }>
+                    By Andrew Ray
+                </div>
+            </div>
+
         </div>;
 
     }
